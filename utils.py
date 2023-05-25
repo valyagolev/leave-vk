9d431c0e66fd22f5f85638da8f3a7f0950eabfc0
geeeezmo <gizmoptz@gmail.com>
2023-05-25 22:21:14 +0300

    Add logging of album/photo counters, extract directory creation to a utility function

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 import datetime
 import pytz
+import os
 
 def timestamp_to_moscow_datetime(ts):
     local_tz = pytz.timezone('Europe/Moscow')
     dt = datetime.datetime.utcfromtimestamp(ts).replace(tzinfo=datetime.timezone.utc)
     return dt.astimezone(local_tz)
+
+def create_dir(dir):
+    try:
+        os.makedirs(dir)
+    except FileExistsError:
+        # print('directory %s already exists, skipping' % dir)
+        pass
